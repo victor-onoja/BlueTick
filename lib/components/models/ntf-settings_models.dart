4a3fb31ed56b4b867ade5ac91c03c3e3aecef1e1
dimeji <opeyemioladimeji.m@gmail.com>
2022-07-28 17:57:56 +0100

    Bluetick

---
+++ lib/components/models/ntf-settings_models.dart
@@ -0,0 +1,44 @@
+Map<String, Map> nSettingItems = {
+  'Messages': {
+    'Private Chats': [
+      ['Default tone'],
+      'Default tone'
+    ],
+    'Groups': [
+      ['Default tone'],
+      'Default tone'
+    ],
+    'Broadcasts': [
+      ['Clashing symbals'],
+      'Clashing symbals'
+    ],
+    'Vibration': [
+      ['None', 'Default', 'Long', 'Short'],
+      'Default'
+    ],
+    'Light': [
+      ['None', 'White', 'Red', 'Blue', 'Green'],
+      'Blue'
+    ],
+    'Use High Priority Notifications':
+    'pop-up previews of message at the top of the screen'
+  },
+  'Calls': {
+    'Private Chats': [
+      ['Default tone'], //options
+      'Default tone'
+    ], //groupValue
+    'Groups': [
+      ['Default tone'],
+      'Default tone'
+    ],
+    'Vibration': [
+      ['None', 'Default', 'Long', 'Short'],
+      'None'
+    ],
+    'Light': [
+      ['Default'],
+      'Default'
+    ]
+  }
+};
