a427058ffc904107eb4a65283cacfec8885f0eee
victor-onoja <odohvictor47@gmail.com>
2022-08-18 00:05:09 +0100

    api base files created

---
+++ lib/components/services/signup_api_model.dart
@@ -0,0 +1,29 @@
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
+
+import 'dart:convert';
+
+Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+
+String welcomeToJson(Welcome data) => json.encode(data.toJson());
+
+class Welcome {
+  Welcome({
+    required this.message,
+    required this.token,
+  });
+
+  String message;
+  String token;
+
+  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+        message: json["message"],
+        token: json["token"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "message": message,
+        "token": token,
+      };
+}
